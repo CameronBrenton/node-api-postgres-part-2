7809b59d8b9b08c089bb357c564a001a1fa4512c
CameronBrenton <Cameron.Brenton@keyin.com>
2021-06-06 10:54:15 -0230

    added contoller

---
+++ controllers/users/index.js
@@ -0,0 +1,27 @@
+const createUser = async (request, response) => {
+
+}
+
+const getUsers = async (request, response) => {
+    
+}
+
+const getUserById = async (request, response) => {
+    
+}
+
+const updateUser = async (request, response) => {
+    
+}
+
+const deleteUser = async (request, response) => {
+    
+}
+
+module.exports = {
+    createUser,
+    getUsers,
+    getUserById,
+    updateUser,
+    deleteUser
+}
