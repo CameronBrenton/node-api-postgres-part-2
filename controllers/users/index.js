8ed3e86b8cbbc614191c9ad3ebdff79fa73703a5
CameronBrenton <Cameron.Brenton@keyin.com>
2021-06-06 12:25:12 -0230

    12:25pm update

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -7,18 +7,29 @@ const createUser = async (request, response) => {
 
     try{
         const res = await model.create(name, email);
-        response.status(201).send()
+        response.status(201).send(res)
     } catch(error) {
-        
+        response.status(500).send(error);
     }
 }
 
 const getUsers = async (request, response) => {
-    
+    try{
+        const res = await model.findAll();
+        response.status(200).json(res);
+    } catch(error) {
+        response.status(500).send(error);
+    }
 }
 
 const getUserById = async (request, response) => {
-    
+    const userID = parseInt(request.params.id);
+    try{
+        const res = await model.findOne(userID);
+        response.status(200).json(res);
+    } catch(error) {
+        response.status(500).send(error);
+    }
 }
 
 const updateUser = async (request, response) => {
