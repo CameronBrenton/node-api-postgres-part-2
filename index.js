36266b20a57efeaec902b0ed6728a1669078ba11
CameronBrenton <Cameron.Brenton@keyin.com>
2021-06-06 14:34:00 -0230

    2:33pm commit. Finished.

--- index.js
+++ index.js
@@ -8,15 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
 
-/*
-app.post('/users', db.createUser);
-app.get('/users', db.getUsers);
-app.get('/users/:id', db.getUsersById);
-app.put('/users/:id', db.updateUser);
-app.delete('/users/:id', db.deleteUser);
-*/
-
-
 app.listen(port, () => {
     console.log(`Running on http://localhost:${port}`);
 })
