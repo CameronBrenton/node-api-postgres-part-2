619adf61eaedbc3713f5e92862542041742a43a1
CameronBrenton <Cameron.Brenton@keyin.com>
2021-06-06 10:52:25 -0230

    updated user routes

--- users/index.js
+++ users/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-app.post('/users', db.createUser);
-app.get('/users', db.getUsers);
-app.get('/users/:id', db.getUsersById);
-app.put('/users/:id', db.updateUser);
-app.delete('/users/:id', db.deleteUser);
+const usersController = require('../../controllers');
+
+
+
+router.post('/', usersController.createUser);
+router.get('/', usersController.getUsers);
+router.get('/:id', usersController.getUsersById);
+router.put('/:id', usersController.updateUser);
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
